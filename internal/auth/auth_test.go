626e8c244d1551ab1cdbf7c7d487999aa48ff373
Bilal Kanjelkheir <bilal.kanjelkheir@gmail.com>
2025-08-17 21:18:22 +0300

    Add a unit test for the GetBearerFunction in auth.go

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"net/http"
 	"testing"
 	"time"
 
@@ -96,5 +97,39 @@ func TestValidateJWT(t *testing.T) {
 	_, err = ValidateJWT(expiredTokenString, tokenSecret)
 	if err == nil {
 		t.Error("ValidateJWT should have failed with an expired token, but it didn't")
+	}
+}
+
+func TestGetBearerToken(t *testing.T) {
+	value := "Bearer wg3yoZ6PSOEu5ST8Fd4ue7064Rk57EeeXMBuUwpKwTPplEwnW/KlHnvATIm5dAS9xGOz04vjdEx0rn4tx4zbjg=="
+	header := make(http.Header)
+	header.Set("Authorization", value)
+
+	foundToken, err := GetBearerToken(&header)
+	if err != nil {
+		t.Errorf("GetBearerToken failed with valid token: %v", err)
+	}
+	if foundToken != "wg3yoZ6PSOEu5ST8Fd4ue7064Rk57EeeXMBuUwpKwTPplEwnW/KlHnvATIm5dAS9xGOz04vjdEx0rn4tx4zbjg==" {
+		t.Errorf("GetBearerToken returned incorrect token. Expected: %s, Got: %s", "wg3yoZ6PSOEu5ST8Fd4ue7064Rk57EeeXMBuUwpKwTPplEwnW/KlHnvATIm5dAS9xGOz04vjdEx0rn4tx4zbjg==", foundToken)
+	}
+
+	emptyHeader := make(http.Header)
+	_, err = GetBearerToken(&emptyHeader)
+	if err == nil {
+		t.Error("GetBearerToken should have failed with missing header, but it didn't")
+	}
+
+	malformedHeader1 := make(http.Header)
+	malformedHeader1.Set("Authorization", "Basic some_token")
+	_, err = GetBearerToken(&malformedHeader1)
+	if err == nil {
+		t.Error("GetBearerToken should have failed with malformed header (no Bearer), but it didn't")
+	}
+
+	malformedHeader2 := make(http.Header)
+	malformedHeader2.Set("Authorization", "Bearer")
+	_, err = GetBearerToken(&malformedHeader2)
+	if err == nil {
+		t.Error("GetBearerToken should have failed with malformed header (Bearer no token), but it didn't")
 	}
 }
