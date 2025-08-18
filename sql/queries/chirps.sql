9248c5823676ddeee3986b3ac51da09f76556ff9
Bilal Kanjelkheir <bilal.kanjelkheir@gmail.com>
2025-08-18 16:05:13 +0300

    Add chirps commands

---
+++ sql/queries/chirps.sql
@@ -0,0 +1,15 @@
+-- name: CreateChirp :one
+insert into chirps (id, created_at, updated_at, body, user_id) values (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5
+) returning *;
+
+
+-- name: GetChirps :many
+select * from chirps order by created_at asc;
+
+-- name: GetChirp :one
+select * from chirps where id = $1;
